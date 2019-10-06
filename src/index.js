f985f21af6051fcf8e6f6fa9c7f9b8a010e459c7
Volha Soika <volhasoika@gmail.com>
2019-10-06 12:28:19 +0300

    add solution

--- src/index.js
+++ src/index.js
@@ -37,9 +37,32 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
-function decode(expr) {
-    // write your solution here
-}
+    function decode(expr) {
+        let partOfString = [];
+        let result = [];
+    
+        for (let i = 0 ; i < expr.length; i += 10) {
+            partOfString.push(expr.substr(i, 10));
+        }
+        
+        for (let j = 0 ; j < partOfString.length ; j ++) {
+            let decodeToMorse = partOfString[j]
+                .replace(/11/gi, '-')
+                .replace(/10/gi, '.')
+                .replace(/0/gi, '')
+                
+            function findKey (key){
+                return decodeToMorse === key;
+            }
+            let morseKey = Object.keys(MORSE_TABLE).find(findKey);
+            let decodeToEnglish = MORSE_TABLE[morseKey];
+    
+            result.push(decodeToEnglish || ' ');
+    
+    }
+        return result.join('');
+    }
+
 
 module.exports = {
     decode
